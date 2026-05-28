4ade5d7ca7cf58887ad9b257cd3b4899d3911780
xy170 <ycwlgzs@gmail.com>
2026-05-29 00:56:27 +0800

    Update as.php

--- as.php
+++ as.php
@@ -1,35 +1,14 @@
 <?php
 
-// $domain = $_SERVER['HTTP_HOST'] ?? 'unknown';
-// $time = date('Y-m-d H:i:s');
-
-// $data = [
-//     'domain' => $domain,
-//     'time'   => $time
-// ];
-
-// $ch = curl_init('http://10.163.17.203/api/stat/collect');
-
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_POST, true);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-// curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-
-// $response = curl_exec($ch);
-// curl_close($ch);
-// // file_put_contents(__DIR__ . '/stat.log', "domain={$domain}, time={$time}\n", FILE_APPEND);
-// // echo 'ok';
-// return;
-
-
-
 $domain = $_SERVER['HTTP_HOST'] ?? 'unknown';
+$time = date('Y-m-d H:i:s');
 
 $data = [
-    'domain' => $domain
+    'domain' => $domain,
+    'time'   => $time
 ];
 
-$ch = curl_init('http://127.0.0.1/index.php?s=/api/stat/collect');
+$ch = curl_init('http://10.163.17.203/api/stat/collect');
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
@@ -37,12 +16,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 
 $response = curl_exec($ch);
-$error = curl_error($ch);
 curl_close($ch);
-
-file_put_contents(__DIR__ . '/stat.log',
-    "response={$response}, error={$error}\n",
-    FILE_APPEND
-);
-
+// file_put_contents(__DIR__ . '/stat.log', "domain={$domain}, time={$time}\n", FILE_APPEND);
+// echo 'ok';
 return;
